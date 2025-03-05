693555be0d24c5bb1f8c9bc7c00ddb7e2321d9dd
firoliyos <firatesfaye44@gmail.com>
2025-03-05 11:34:27 +0300

    ProjectDetails.js-done

--- firaplan/src/App.js
+++ firaplan/src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
-
+import ProjectDetails from "./components/projects/ProjectDetails";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar/>
         <Routes>
-          <Route path='/' element={<Dashboard />}/>
+          <Route exact path='/' element={<Dashboard />}/>
+          <Route path='/project/:id' element={<ProjectDetails />}/>
         </Routes>
       </div>
     </BrowserRouter>
--- firaplan/src/components/projects/ProjectDetails.js
+++ firaplan/src/components/projects/ProjectDetails.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-function ProjectDetails() {
+const ProjectDetails = () => {
+  const { id } = useParams()
+
   return (
-    <div>ProjectDetails</div>
+    <div className='container section project-details'>
+      <div className='card z-depth-0'>
+        <div className='card-content'>
+          <span className='card-title'>Project Title - {id} </span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className='card-action grey lighten-4 grey-text'>
+          <div>Posted by The Net Ninja</div>
+          <div>2nd September, 2am</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
