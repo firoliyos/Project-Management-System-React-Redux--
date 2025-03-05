b99fd262032ff9a5d180ce0530da8aace2565081
firoliyos <firatesfaye44@gmail.com>
2025-03-05 11:56:11 +0300

    SignIn.js-done

--- firaplan/src/App.js
+++ firaplan/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Dashboard />}/>
           <Route path='/project/:id' element={<ProjectDetails />}/>
+          <Route path='/signin' element={<SignIn />}/>
         </Routes>
       </div>
     </BrowserRouter>
