8509bff91ee72aa52c850e28ddd1255390490684
firoliyos <firatesfaye44@gmail.com>
2025-03-11 09:52:20 +0300

    redux-thunk-added

--- firaplan/src/index.js
+++ firaplan/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
 
-
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ firaplan/src/store/actions/projectActions.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const createProject = (project) => {
+  return (dispatch, getState) => {
+    // make async call to database
+    dispatch({ type: 'CREATE_PROJECT', project });
+  }
+}
+
